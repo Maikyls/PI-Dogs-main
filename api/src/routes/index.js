c3c72cf99ea8e4215dfbf30cc8bae4e22d841bb8
Maikyls <85658257+Maikyls@users.noreply.github.com>
2022-02-17 03:49:10 -0500

    Initial commit

---
+++ api/src/routes/index.js
@@ -0,0 +1,14 @@
+const { Router } = require('express');
+// Importar todos los routers;
+// Ejemplo: const authRouter = require('./auth.js');
+
+
+const router = Router();
+
+// Configurar los routers
+// Ejemplo: router.use('/auth', authRouter);
+router.get('/', (req, res) => {
+    res.send('Hello World');
+})
+
+module.exports = router;
